317bc47305d100067e6d15d5c2388697122e28d7
claire abadji <claire.abadji@gmail.com>
2020-12-30 19:56:33 +0100

    trying to debug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::API
   include JWTSessions::RailsAuthorization
-  rescue_from JWTSessions::Errors::Unauthorized, with: :not_authorized 
+  rescue_from JWTSessions::Errors::Unauthorized, with: :not_authorized
 
   private
-  
+
   def current_user
     @current_user ||= User.find(payload['user_id'])
   end
